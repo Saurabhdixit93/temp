582895c703a8f8b81a8634826d7a8a59175e8ff2
Saurabh Dixit <86460997+Saurabhdixit93@users.noreply.github.com>
2023-01-18 19:23:14 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,52 +1,160 @@
-/*
-		Designed by: michaelyeah
-		Original image: https://dribbble.com/shots/3661410-walkman
-*/
+var ball = document.getElementById('ball');
+var rod1 = document.getElementById('rod1');
+var rod2 = document.getElementById('rod2');
 
-const b = document.body
-const h = document.querySelector("#h")
-const unit = 1.75
-const audioElement = document.querySelector("#a")
-const btnElement = document.querySelector(".button5")
-const tapeElement = document.querySelector(".c")
-const circleElement = document.querySelectorAll(".circle")[1]
-const waveElement = document.querySelector(".waves")
-// =================================
-const mouseDown = () => b.addEventListener('mousemove', moveFunc)
-const mouseUp = () => b.removeEventListener('mousemove', moveFunc)
-const moveFunc = (e) => {
-    let x = e.pageX / window.innerWidth - 0.5
-    let y = e.pageY / window.innerHeight - 0.5
 
-    h.style.transform = `
-        perspective(${400 * unit}vmin)
-        rotateX(${ y * 20 + 66}deg)
-        rotateZ(${ -x * 420 + 40}deg)
-        translateZ(${-16 * unit}vmin)
-    `
+const storeName = "PPName";
+const storeScore = "PPMaxScore";
+const rod1Name = "Rod 1";
+const rod2Name = "Rod 2";
+
+
+let score,
+    maxScore,
+    movement,
+    rod,
+    ballSpeedX = 2,
+    ballSpeedY = 2;
+
+let gameOn = false;
+
+let windowWidth = window.innerWidth,
+    windowHeight = window.innerHeight;
+
+
+
+(function () {
+    rod = localStorage.getItem(storeName);
+    maxScore = localStorage.getItem(storeScore);
+
+    if (rod === "null" || maxScore === "null") {
+        alert("This is the first time you are playing this game. LET'S START");
+        maxScore = 0;
+        rod = "Rod1"
+    } else {
+        alert(rod + " has maximum score of " + maxScore * 100);
+    }
+
+    resetBoard(rod);
+})();
+
+
+
+function resetBoard(rodName) {
+
+    rod1.style.left = (window.innerWidth - rod1.offsetWidth) / 2 + 'px';
+    rod2.style.left = (window.innerWidth - rod2.offsetWidth) / 2 + 'px';
+    ball.style.left = (windowWidth - ball.offsetWidth) / 2 + 'px';
+
+
+    // Lossing player gets the ball
+    if (rodName === rod2Name) {
+        ball.style.top = (rod1.offsetTop + rod1.offsetHeight) + 'px';
+        ballSpeedY = 2;
+    } else if (rodName === rod1Name) {
+        ball.style.top = (rod2.offsetTop - rod2.offsetHeight) + 'px';
+        ballSpeedY = -2;
+    }
+
+    score = 0;
+    gameOn = false;
+
 }
-const playRadio = () => {
-    btnElement.removeEventListener("click", playRadio)
-    audioElement.currentTime = 0
-    audioElement.play()
-    setTimeout(() => waveElement.classList.add('is-wave-playing'), 1100);
-    toggleStyles()
 
-}
-const stopRadio = () => {
-    btnElement.addEventListener("click", playRadio)
-    audioElement.pause()
-    waveElement.classList.remove('is-wave-playing')
-    toggleStyles()
-}
-const toggleStyles = () => {
-    btnElement.classList.toggle('is-button-pressed')
-    tapeElement.classList.toggle('is-c-close')
-    circleElement.classList.toggle('cr')
+
+
+function storeWin(rod, score) {
+
+    if (score > maxScore) {
+        maxScore = score;
+        localStorage.setItem(storeName, rod);
+        localStorage.setItem(storeScore, maxScore);
+    }
+
+    clearInterval(movement);
+    resetBoard(rod);
+
+    alert(rod + " wins with a score of " + (score * 100) + ". Max score is: " + 
+        (maxScore * 100));
 
 }
 
-h.addEventListener('mousedown', mouseDown)
-b.addEventListener('mouseup', mouseUp)
-btnElement.addEventListener("click", playRadio)
-audioElement.addEventListener('ended', stopRadio)
+
+
+window.addEventListener('keypress', function () {
+    let rodSpeed = 20;
+
+    let rodRect = rod1.getBoundingClientRect();
+
+
+    if (event.code === "KeyD" && ((rodRect.x + rodRect.width) < window.innerWidth)) {
+        rod1.style.left = (rodRect.x) + rodSpeed + 'px';
+        rod2.style.left = rod1.style.left;
+    } else if (event.code === "KeyA" && (rodRect.x > 0)) {
+        rod1.style.left = (rodRect.x) - rodSpeed + 'px';
+        rod2.style.left = rod1.style.left;
+    }
+
+
+    if (event.code === "Enter") {
+
+        if (!gameOn) {
+            gameOn = true;
+            let ballRect = ball.getBoundingClientRect();
+            let ballX = ballRect.x;
+            let ballY = ballRect.y;
+            let ballDia = ballRect.width;
+
+            let rod1Height = rod1.offsetHeight;
+            let rod2Height = rod2.offsetHeight;
+            let rod1Width = rod1.offsetWidth;
+            let rod2Width = rod2.offsetWidth;
+
+
+            movement = setInterval(function () {
+                // Move ball 
+                ballX += ballSpeedX;
+                ballY += ballSpeedY;
+
+                rod1X = rod1.getBoundingClientRect().x;
+                rod2X = rod2.getBoundingClientRect().x;
+
+                ball.style.left = ballX + 'px';
+                ball.style.top = ballY + 'px';
+
+
+                if ((ballX + ballDia) > windowWidth || ballX < 0) {
+                    ballSpeedX = -ballSpeedX; // Reverses the direction
+                }
+
+                // It specifies the center of the ball on the viewport
+                let ballPos = ballX + ballDia / 2;
+
+                // Check for Rod 1
+                if (ballY <= rod1Height) {
+                    ballSpeedY = -ballSpeedY; // Reverses the direction
+                    score++;
+
+                    // Check if the game ends
+                    if ((ballPos < rod1X) || (ballPos > (rod1X + rod1Width))) {
+                        storeWin(rod2Name, score);
+                    }
+                }
+
+                // Check for Rod 2
+                else if ((ballY + ballDia) >= (windowHeight - rod2Height)) {
+                    ballSpeedY = -ballSpeedY; // Reverses the direction
+                    score++;
+
+                    // Check if the game ends
+                    if ((ballPos < rod2X) || (ballPos > (rod2X + rod2Width))) {
+                        storeWin(rod1Name, score);
+                    }
+                }
+
+            }, 10);
+
+        }
+    }
+
+});
